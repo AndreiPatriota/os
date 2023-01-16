90a3a5cf44d5e371620b07b0e378156034251dc5
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2023-01-16 19:51:32 -0300

    Implements object not found error handling
    Implements the error handling for object not found exception, that happens when the API consumer tries to fetch some data that does not exist

--- src/main/java/com/drei/os/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/drei/os/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,8 +1,23 @@
 package com.drei.os.controllers.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.drei.os.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
     
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e) {
+
+        var error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+
 }
--- src/main/java/com/drei/os/controllers/exceptions/StandardError.java
+++ src/main/java/com/drei/os/controllers/exceptions/StandardError.java
@@ -1,5 +1,43 @@
 package com.drei.os.controllers.exceptions;
 
-public class StandardError {
-    
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+    private static final Long serialVersionUID = 1L;
+    private Long timestamp;
+    private Integer status;
+    private String error;
+
+    public StandardError(Long timestamp, Integer status, String error) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+    }
+
+    public StandardError() {
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
